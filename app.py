b300e96a5f0fa2ddc9fe3db9c4d067722dd67af3
“ <andrewdossantos38@gmail.com>
2024-07-20 18:44:07 -0300

    feat:corrigindo-app-para-conexao-banco-docker

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hoteis,Hotel
-#começando com o flask
+from resources.hotel import Hoteis, Hotel
+from sql_alquemy import banco  # Importe o SQLAlchemy
+
 app = Flask(__name__)
+
+# Configuração do SQLAlchemy server se for um docker compose
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:32133@localhost:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 api = Api(app)
 
-api.add_resource(Hoteis,'/Hoteis')
-api.add_resource(Hotel,'/hoteis/<string:hotel_id>')
+# Inicialização do SQLAlchemy com o aplicativo Flask
+banco.init_app(app)
+
+# Chamada para criar todas as tabelas uma vez na primeira requisiçao
+with app.app_context():
+    banco.create_all()
+
+# Definindo os endpoints da API
+api.add_resource(Hoteis, '/Hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
